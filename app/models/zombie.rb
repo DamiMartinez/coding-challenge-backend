262e383ba114dd9e641b9200b5184708cfa5823d
DamianMartinez <damian.martinez.carmona@gmail.com>
2018-04-02 22:18:15 +0200

    Uncommenting validations

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -3,8 +3,8 @@ class Zombie < ApplicationRecord
   has_many :zombie_weapon, dependent: :destroy
 
   # You must put a name and it must be unique
-  # validates :name, presence: true,
-  #                  uniqueness: { case_sensitive: false }
+  validates :name, presence: true,
+                   uniqueness: { case_sensitive: false }
 
   def self.search_by_weapon_armor(name, hit_points, brains_eaten, speed, weapon_id, armor_id)
     where("zombies.name LIKE ? AND zombies.hit_points LIKE ? AND zombies.brains_eaten LIKE ? AND zombies.speed LIKE ? AND zombie_weapons.weapon_id LIKE ? AND zombie_armors.armor_id LIKE ?", "%#{name}%", "%#{hit_points}%", "%#{brains_eaten}%", "%#{speed}%", "%#{weapon_id}%", "%#{armor_id}%")
