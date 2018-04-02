fa707f023c4e7354592ed3fff81937352a727710
DamianMartinez <damian.martinez.carmona@gmail.com>
2018-04-02 22:16:05 +0200

    Commenting validations on models to seed db

--- app/models/armor.rb
+++ app/models/armor.rb
@@ -2,6 +2,6 @@ class Armor < ApplicationRecord
   has_many :zombie_armor, dependent: :destroy
 
   # You must put a name and it must be unique
-  validates :name, presence: true,
-                   uniqueness: { case_sensitive: false }
+  # validates :name, presence: true,
+  #                  uniqueness: { case_sensitive: false }
 end
--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -2,6 +2,6 @@ class Weapon < ApplicationRecord
   has_many :zombie_weapon, dependent: :destroy
 
   # You must put a name and it must be unique
-  validates :name, presence: true,
-                   uniqueness: { case_sensitive: false }
+  # validates :name, presence: true,
+  #                  uniqueness: { case_sensitive: false }
 end
